7770d73ffabbe120dd76c1b6502b3694449ba0a7
ahipoki <50777654+ahipoki@users.noreply.github.com>
2020-04-24 16:44:27 -0700

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,31 @@
+#ifndef NODE_H
+#define NODE_H
+#include <iostream>
+
+class Node{
+  public:
+    Node(int);
+    int getKey();
+    void setKey(int);
+    Node* getLeft();
+    void setLeft(Node*);
+    Node* getRight();
+    void setRight(Node*);
+    Node* getParent();
+    void setParent(Node*);
+    Node* getUncle();
+    Node* getSibling();
+    void toggleColor();
+    void rotateLeft();
+    void rotateRight();
+    bool isRed();
+    ~Node();
+  private:
+    Node* Left;
+    Node* Right;
+    Node* Parent;
+    int key;
+    bool isred;
+};
+
+#endif
